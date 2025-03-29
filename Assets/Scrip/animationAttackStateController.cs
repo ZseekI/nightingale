889fed777d2c5852cd3ddb331fd3be9105a98fa0
ZseekI <newzee09122@gmail.com>
2025-03-29 15:16:37 +0700

    Prototype Nightingale v 0.3.1
    Update Ex. animation system

--- Assets/Scrip/animationAttackStateController.cs
+++ Assets/Scrip/animationAttackStateController.cs
@@ -5,7 +5,12 @@ public class animationAttackStateController : MonoBehaviour
     private Animator _animatorPlayer;
     public int CountAttackClick;
     public float attackResetTime = 1.0f;
-    private float resetTime;
+    public float idleResetTime = 1.0f;
+    private float resetAttackTime;
+    private float resetIdleTime;
+    int isWalkingHash;
+    int isRunHash;
+    public int T = 5;
     void Start()
     {
         FindPlayer();
@@ -15,6 +20,8 @@ public class animationAttackStateController : MonoBehaviour
     {
         _animatorPlayer = GameObject.FindGameObjectWithTag("PlayerMesh").GetComponent<Animator>();
         CountAttackClick = 0;
+        isWalkingHash = Animator.StringToHash("isWalking");
+        isRunHash = Animator.StringToHash("isRun");
         
     }
     
@@ -24,19 +31,46 @@ public class animationAttackStateController : MonoBehaviour
 
         _animatorPlayer.SetInteger("intAttackPhase", 1);
 
-        if (CountAttackClick >= 16)
-        {
-            ResetAttackPhase();
-        }
+        resetAttackTime = attackResetTime;
         
     }
 
         private void Update()
     {
+        bool isWalking = _animatorPlayer.GetBool(isWalkingHash);
+        bool isRun = _animatorPlayer.GetBool(isRunHash);
+
         if (_animatorPlayer == null)
         {
             FindPlayer(); // I need it to find player mesh if at start dont
         }
+
+        if(resetAttackTime > 0)
+        {
+            resetAttackTime = Mathf.Max(resetAttackTime - Time.deltaTime, 0);
+            if (resetAttackTime == 0 && CountAttackClick > T)
+            {
+                Debug.Log("resetAttackTime"+resetAttackTime);
+                CheckedAttackPhase();
+            }
+        }
+
+        
+        resetIdleTime = Mathf.Max(resetIdleTime - Time.deltaTime, 0);
+        if (resetIdleTime > 0 && !isWalking && !isRun) 
+            {
+                _animatorPlayer.SetBool("isIdle", true);
+            }
+        
+        else
+            { 
+                    resetIdleTime = 0;
+                    _animatorPlayer.SetBool("isIdle", false);
+            }
+    
+        
+
+    
     }
 
     public void CheckedAttackPhase()
@@ -45,7 +79,7 @@ public class animationAttackStateController : MonoBehaviour
         if (_animatorPlayer.GetCurrentAnimatorStateInfo(0).IsName("Boxing"))
         {
             Debug.Log("Current State 1");
-            if(CountAttackClick > 1)
+            if(CountAttackClick > 1 && resetAttackTime > 0)
             {
                 _animatorPlayer.SetInteger("intAttackPhase", 2);
             }
@@ -57,7 +91,7 @@ public class animationAttackStateController : MonoBehaviour
         else if (_animatorPlayer.GetCurrentAnimatorStateInfo(0).IsName("Elbow Punch"))
         {
             Debug.Log("Current State 2");
-            if(CountAttackClick > 2)
+            if(CountAttackClick > 2 && resetAttackTime > 0)
             {
                 _animatorPlayer.SetInteger("intAttackPhase", 3);
             }
@@ -69,7 +103,7 @@ public class animationAttackStateController : MonoBehaviour
         else if (_animatorPlayer.GetCurrentAnimatorStateInfo(0).IsName("Hook Punch"))
         {
             Debug.Log("Current State 3");
-            if(CountAttackClick > 3)
+            if(CountAttackClick > 3 && resetAttackTime > 0)
             {
                 _animatorPlayer.SetInteger("intAttackPhase", 4);
             }
@@ -81,7 +115,7 @@ public class animationAttackStateController : MonoBehaviour
         else if (_animatorPlayer.GetCurrentAnimatorStateInfo(0).IsName("Headbutt"))
         {
             Debug.Log("Current State 4");
-            if(CountAttackClick > 4)
+            if(CountAttackClick > 4 && resetAttackTime > 0)
             {
                 _animatorPlayer.SetInteger("intAttackPhase", 5);
             }
@@ -93,17 +127,19 @@ public class animationAttackStateController : MonoBehaviour
         else if (_animatorPlayer.GetCurrentAnimatorStateInfo(0).IsName("Fist Fight A"))
         {
             Debug.Log("Current State 5");
-            if (CountAttackClick >= 5)
+            if (CountAttackClick >= 5 )
             {
                 ResetAttackPhase();
             }
         }
-
     }
 
     private void ResetAttackPhase()
     {
+
         CountAttackClick = 0;
+        resetIdleTime = idleResetTime;
         _animatorPlayer.SetInteger("intAttackPhase", 0);
+        
     }
 }
